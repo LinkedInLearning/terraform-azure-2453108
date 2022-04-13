56925001a2cf7f5a49b8e659a9314ac3048afaf8
Alexandra Illarionov <34364655+a-illarionov@users.noreply.github.com>
2022-04-13 11:58:57 -0500

    Create NICs

--- main.tf
+++ main.tf
@@ -35,3 +35,16 @@ resource "azurerm_subnet" "main" {
   resource_group_name = azurerm_resource_group.main.name 
   address_prefixes = ["10.0.0.0/24"]
 }
+
+#Creates network interface card (NIC)
+resource "azurerm_network_interface" "internal" {
+  name                = "learn-tf-nic-eastus"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
