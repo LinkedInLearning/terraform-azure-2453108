87255587fc9afa745c1b700fa21b8f16f2c70056
Alexandra Illarionov <34364655+a-illarionov@users.noreply.github.com>
2022-04-13 11:47:00 -0500

    Create main.tf - add resource provider

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.90.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
