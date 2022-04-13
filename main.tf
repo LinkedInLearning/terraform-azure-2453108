6978105d8a147b25ed68224ec5662b0578435f0d
Alexandra Illarionov <34364655+a-illarionov@users.noreply.github.com>
2022-04-13 11:58:09 -0500

    Create subnets

--- main.tf
+++ main.tf
@@ -27,3 +27,11 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
 }
+
+#Create subnet
+resource "azurerm_subnet" "main" {
+  name = "learn-tf-subnet-eastus"
+  virtual_network_name = azurerm_virtual_network.main.name 
+  resource_group_name = azurerm_resource_group.main.name 
+  address_prefixes = ["10.0.0.0/24"]
+}
