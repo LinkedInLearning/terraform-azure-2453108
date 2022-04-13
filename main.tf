9a4f57737779f0b7b790e04248ba42e606bf9238
Alexandra Illarionov <34364655+a-illarionov@users.noreply.github.com>
2022-04-13 11:50:55 -0500

    updated to add vnets

--- main.tf
+++ main.tf
@@ -19,3 +19,11 @@ resource "azurerm_resource_group" "main" {
   name     = "learn-tf-rg-eastus"
   location = "eastus"
 }
+
+#Creates virtual network
+resource "azurerm_virtual_network" "main" {
+  name                = "learn-tf-vnet-eastus"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
