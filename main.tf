5d14263d237349df4cab74b809f9f15ff39f6fed
Alexandra Illarionov <34364655+a-illarionov@users.noreply.github.com>
2022-05-05 13:17:20 -0500

    reference code for 04_01

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.90.0"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "azurerm" {
-  features {}
+#Creates resource group
+resource "azurerm_resource_group" "main" {
+  name     = "learn-tf-rg-eastus"
+  location = "eastus"
 }
